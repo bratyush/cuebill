31bf108c2d5d1133628e404b8be5dcb1b4eb3d85
Pratyush <conenct2pratyush@gmail.com>
2024-04-25 17:30:11 +0530

    db-calls

--- src/server/backend.ts
+++ src/server/backend.ts
@@ -1,15 +1,14 @@
 import "server-only"
+
 import { db } from "./db"
-import { posts } from "./db/schema";
 
-export async function getPosts(){
-  
-  const allPosts = await db.query.posts.findMany();
-  console.log('allPosts', allPosts);
-  return allPosts;
-}
 
-export async function addPost(name: string) {
-  await db.insert(posts).values({name: name})
+export async function getBills() {
+  const bills = await db.query.bills.findMany({
+    with: {
+      table: true
+    }
+  })
 
+  return bills;
 }
