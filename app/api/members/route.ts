ab50df894942e42579f8f3580099b94d1a9d8a5e
pratyush3124 <connect2pratyush@gmail.com>
2025-09-05 23:00:11 +0530

    same

--- app/api/members/route.ts
+++ app/api/members/route.ts
@@ -9,7 +9,7 @@ export async function POST(request: Request) {
   const body = await request.json() as MemberType;
 
   const user = await currentUser();
-  const club: string = user?.publicMetadata?.org ?? '';
+  const club = user?.publicMetadata?.org as string ?? '';
 
   await db.insert(members).values({ ...body, club: club });
 
@@ -22,7 +22,7 @@ export async function GET() {
 
   if (!user) return Response.json({ members: [] });
 
-  const club: string = user?.publicMetadata?.org ?? "";
+  const club = user?.publicMetadata?.org as string ?? '';
 
   const allMembers = await db.query.members.findMany({
     where: eq(members.club, club),
